a64b4280ebae9487b0f16fd46fbd47e863dc5033
ingrydc <ingrydcamilly15@gmail.com>
2024-12-06 13:24:05 -0300

    correção de erro

--- numPrimo.py
+++ numPrimo.py
@@ -1,11 +1,11 @@
-num = int(input(‘Digite um número: ‘))
+num = int(input('Digite um número: '))
 total = 0
 
-for i in range(1, num + 1)
+for i in range(1, num + 1):
     if num % i == 0:
         total += 1
 
 if total == 2:
-    print(f’{num} é um número primo’)
+    print(f'{num} é um número primo')
 else:
-    print(f’{num} não é um número primo’)
+    print(f'{num} não é um número primo')
