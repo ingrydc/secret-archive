25ae791e5f1b4b143d83d67d17c819edaf9076e6
ingrydc <116104947+ingrydc@users.noreply.github.com>
2024-12-06 00:27:44 -0300

    Create saque.py

---
+++ saque.py
@@ -0,0 +1,17 @@
+def saque(valor):
+    notas = [100, 50, 20, 10]
+
+    for nota in notas:
+        quantidade = int(valor // nota)
+        valor -= quantidade * nota
+
+        print(f’{quantidade} notas de R${nota}’)
+
+    if valor > 0:
+        print(‘Restam {valor} centavos.’)
+    else:
+        print(‘Saque realizado com sucesso!’)
+
+
+valor = int(input(‘Digite o valor do saque: R$’)
+saque(valor)
