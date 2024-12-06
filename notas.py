3304a64c532a0a05d761e619480736436f745619
ingrydc <ingrydcamilly15@gmail.com>
2024-12-06 13:45:18 -0300

    correção questao 11

---
+++ notas.py
@@ -0,0 +1,18 @@
+nota = float(input('Digite sua nota: '))
+faltas = int(input('Digite o número de faltas: '))
+
+if faltas <= 10:
+    if nota < 60:
+        print('Seu conceito é C.')
+    elif nota >= 60 and nota < 85:
+        print('Seu conceito é B.')
+    else:
+        print('Seu conceito é A.')
+
+else:
+    if nota < 60:
+        print('Seu conceito é D.')
+    elif nota >= 60 and nota < 85:
+        print('Seu conceito é C.')
+    else:
+        print('Seu conceito é B.')
